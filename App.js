1a086f07fe265c140bee62ffc0a8147096e9d01a
UllaMontonen <u.montonen@gmail.com>
2023-10-14 15:52:51 +0300

    added react native elements

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, FlatList, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Keyboard } from 'react-native';
 import * as SQLite from 'expo-sqlite';
+import { Header, Input, Button, ListItem, Icon } from '@rneui/themed';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 export default function App() {
@@ -15,23 +17,25 @@ export default function App() {
   // creating a database
   const db = SQLite.openDatabase('shoppinglist.db');
 
+
+
   // creating a table
   useEffect(() => {
     db.transaction(tx => {
-      tx.executeSql('create table if not exists data (id integer primary key not null, product text, amount text);'); 
-    }, 
-    () => console.error("Error when creating DB"), updateList);
+      tx.executeSql('create table if not exists data (id integer primary key not null, product text, amount text);');
+    },
+      () => console.error("Error when creating DB"), updateList);
   }, []);
 
   // adding things to shopping list and clearing inputs
   const saveItem = () => {
-      db.transaction(tx => {
-        tx.executeSql('insert into data (product, amount) values (?, ?);',
-          [product, amount]);
-      }, null, updateList)
-      setProduct('');
-      setAmount('');
-      Keyboard.dismiss();
+    db.transaction(tx => {
+      tx.executeSql('insert into data (product, amount) values (?, ?);',
+        [product, amount]);
+    }, null, updateList)
+    setProduct('');
+    setAmount('');
+    Keyboard.dismiss();
   }
 
   // updating shopping list everytime changes are made to the shopping list
@@ -40,81 +44,65 @@ export default function App() {
       tx.executeSql('select * from data;', [], (_, { rows }) =>
         setData(rows._array)
       );
-      
+
     }, null, null);
   }
 
   // deleting an item from the shopping list
   const deleteItem = (id) => {
     db.transaction(
-    tx => { 
-      tx.executeSql('delete from data where id = ?;', [id]);
-    }, 
-    null, 
-    updateList)
+      tx => {
+        tx.executeSql('delete from data where id = ?;', [id]);
+      },
+      null,
+      updateList)
   }
 
   return (
-    <View style={styles.container}>
-      <TextInput 
-          placeholder='Product'
-          style={styles.input} 
-          onChangeText={product => setProduct(product)} 
-          value={product} />
-          <TextInput 
-          placeholder='Amount'
-          style={styles.input} 
-          onChangeText={amount => setAmount(amount)} 
-          value={amount} />
-      <View style={styles.button}>
-            <Button onPress={saveItem} title="SAVE" />
-      </View>
-      <Text style={styles.title}>Shopping List</Text>
-      <FlatList style={styles.list}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => 
-          <View style={styles.list}>
-            <Text>{item.product}, {item.amount}</Text>
-            <Text style={styles.delete} onPress={() => deleteItem(item.id)}>  done</Text>
-          </View>}
+    <SafeAreaProvider>
+    <View>
+      <Header
+        centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff', fontSize: 18, fontWeight: 'bold', marginBottom: 10} }}
+      />
+      <Input
+        placeholder='Product'
+        label='Write product here'
+        style={styles.input}
+        onChangeText={product => setProduct(product)}
+        value={product} />
+      <Input
+        placeholder='Amount'
+        label='Write amount here'
+        style={styles.input}
+        onChangeText={amount => setAmount(amount)}
+        value={amount} />
+        <Button raised icon={{name: 'save', color: "white"}} onPress={saveItem} title="SAVE" />
+      <FlatList 
         data={data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) =>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+          <View style={styles.flex}>
+            <ListItem.Title>{item.product}</ListItem.Title>
+            <Icon style={styles.icon} name="trash-can-outline" type="material-community" color="red" onPress={() => deleteItem(item.id)}/>
+            </View>
+            <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+          </ListItem.Content>
+          </ListItem>}
       />
       <StatusBar style="auto" />
     </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 80,
+  icon: {
+    justifyContent: "flex-end",
   },
-  input: {
-    marginTop: 5,
-    marginBottom: 5,
-    padding: 5,
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  title: {
-    marginTop: 10,
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  list: {
+  flex:{
     flexDirection: 'row',
-  },
-  delete: {
-    color: "green",
-  },
-  // iPhone button does not have bacground color
-  button: {
-    backgroundColor: "#AED6F1",
-    marginTop: 10,
-    marginBottom: 10,
-  },
+    alignItem: "right",
+  }
 });
